03c061d570e524bd4cd5f1aef996874ea3c56975
김멍몽 <wjdahrrla@gmail.com>
2023-12-19 19:52:38 +0900

    [FIX] SpringSecurity 버전에 맞게 수정

--- src/main/java/capstonedesign/capstonedesign/config/auth/AuthenticationConfig.java
+++ src/main/java/capstonedesign/capstonedesign/config/auth/AuthenticationConfig.java
@@ -1,6 +1,5 @@
 package capstonedesign.capstonedesign.config.auth;
 
-
 import capstonedesign.capstonedesign.service.token.TokenService;
 import capstonedesign.capstonedesign.service.user.UserService;
 import lombok.RequiredArgsConstructor;
@@ -10,36 +9,48 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.CorsConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-@Configuration // 이 클래스를 스프링 설정 클래스로 등록
-@EnableWebSecurity // 이 클래스에 웹 보안 설정을 활성화
-@RequiredArgsConstructor // final 또는 @NonNull 필드 값을 파라미터로 받는 생성자를 생성 (lombok 라이브러리의 어노테이션)
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
 public class AuthenticationConfig {
-    private final UserService userService; // 유저 서비스 의존성 주입
+    private final UserService userService;
     private final TokenService tokenService;
 
-    @Value(("${jwt.secret}")) // application.properties 또는 yml 파일의 jwt.secret 값을 여기에 주입
-    private String secretKey; // JWT 토큰 생성 및 파싱에 사용할 비밀키
+    @Value(("${jwt.secret}"))
+    private String secretKey;
 
-    @Bean // 스프링 컨테이너에 등록할 Bean 객체를 정의
-    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-        return httpSecurity.httpBasic().disable() // 기본 로그인 페이지 비활성화 (토큰 인증을 사용하기 때문)
-                .csrf().disable() // CSRF(Cross-Site Request Forgery) 보호 기능 비활성화
-                .cors().and() // CORS(Cross-Origin Resource Sharing) 설정 활성화
-                .authorizeRequests() // HttpServletRequest를 사용한 접근 제한 설정
-                .antMatchers("/login", "/signup", "/").permitAll() // /login, /signup 경로는 인증 없이 접근 허용
-                .antMatchers(HttpMethod.POST, "/**").authenticated() // 모든 POST 요청은 인증이 필요함
-                .antMatchers(HttpMethod.GET, "/**").authenticated() // 모든 GET 요청도 인증이 필요
-                .and()
-                .sessionManagement() // 세션 관리
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS) // 세션을 사용하지 않고 STATELESS로 설정
-                .and()
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                // stateless한 rest api를 개발할 것이므로 csrf 공격에 대한 옵션은 꺼둔다.
+                .csrf(AbstractHttpConfigurer::disable)
+
+                // 특정 URL에 대한 권한 설정.
+                .authorizeHttpRequests((authorizeRequests) -> {
+                    authorizeRequests.requestMatchers("/user/**").authenticated();
+
+                    authorizeRequests.requestMatchers("/manager/**")
+                            // ROLE_은 붙이면 안 된다. hasAnyRole()을 사용할 때 자동으로 ROLE_이 붙기 때문이다.
+                            .hasAnyRole("ADMIN", "MANAGER");
+
+                    authorizeRequests.requestMatchers("/admin/**")
+                            // ROLE_은 붙이면 안 된다. hasRole()을 사용할 때 자동으로 ROLE_이 붙기 때문이다.
+                            .hasRole("ADMIN");
+
+                    authorizeRequests.anyRequest().permitAll();
+                })
+
+                .formLogin((formLogin) -> {
+                    /* 권한이 필요한 요청은 해당 url로 리다이렉트 */
+                    formLogin.loginPage("/login");
+                })
                 .addFilterBefore(new JwtTokenFilter(userService, secretKey, tokenService), UsernamePasswordAuthenticationFilter.class)
-                // JwtTokenFilter를 UsernamePasswordAuthenticationFilter 전에 추가
-                .build(); // SecurityFilterChain 객체 생성 및 반환
+                .build();
     }
 }
